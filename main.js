c3db6dad88cdf6cbd97b1b1a8b2a376b46eed7b5
administratorid <77772549+administratorid@users.noreply.github.com>
2021-01-22 22:30:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+ flag=1
+      function f()
+      {
+          if(flag==1)
+              {
+                  Tidak.style.top=50
+                 Tidak.style.left=500
+                  flag=2
+              }
+          else if(flag==2)
+              {
+                  Tidak.style.top=200
+                  Tidak.style.left=50
+                  flag=3
+              }
+          else if(flag==3)
+              {
+                  Tidak.style.top=370
+                  Tidak.style.left=500
+                  flag=4
+              }
+          else if(flag==4)
+              {
+                  Tidak.style.top=70
+                  Tidak.style.left=300
+                  flag=5
+              }
+          else if(flag==5)
+              {
+                  Tidak.style.top=370
+                  Tidak.style.left=500
+                  flag=6
+              }
+          else if(flag==6)
+              {
+                  Tidak.style.top=100
+                  Tidak.style.left=430
+                  flag=1
+              }
+      }
